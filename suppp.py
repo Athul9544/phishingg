29d0a442c40aa5f84b5113900b50aff692f53ee9
Athul9544 <athulpreman01@gmail.com>
2025-02-05 13:13:27 +0530

    Update suppp.py

--- suppp.py
+++ suppp.py
@@ -9,18 +9,17 @@ filename="kn.pk"
 with open(path.join(filename),'rb') as f:
     lr=pickle.load(f)
     
-
-Have_At=st.number_input("e")
-URL_Length=st.number_input("ee")
-URL_Depth=st.number_input("r")
-TinyURL=st.number_input("yyy")
-Prefix_Suffix=st.number_input("h")
-DNS_Record=st.number_input("s")
-Web_Traffic=st.number_input("f")
-Domain_Age=st.number_input("ff")
-Domain_End=st.number_input("v")
-iFrame=st.number_input("vv")
-Web_Forwards=st.number_input("fuck")
+Have_At=st.number_input("Have_At")
+URL_Length=st.number_input("URL_Length")
+URL_Depth=st.number_input("URL_Depth")
+TinyURL=st.number_input("TinyURL")
+Prefix_Suffix=st.number_input("Prefix/Suffix")
+DNS_Record=st.number_input("DNS_Record")
+Web_Traffic=st.number_input("Web_Traffic")
+Domain_Age=st.number_input("Domain_Age")
+Domain_End=st.number_input("Domain_End")
+iFrame=st.number_input("iFrame")
+Web_Forwards=st.number_input("Web_Forwards")
 if st.button("predict") :
     pred=lr.predict(np.array([[Have_At,URL_Length,URL_Depth,TinyURL,Prefix_Suffix,DNS_Record,Web_Traffic,Domain_Age,Domain_End,iFrame,Web_Forwards]]))
     if pred==1:
