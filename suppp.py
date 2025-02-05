0a69b44a1080428b962781a3f92e241ccb132874
Athul9544 <athulpreman01@gmail.com>
2025-02-05 13:19:49 +0530

    Update suppp.py

--- suppp.py
+++ suppp.py
@@ -9,20 +9,20 @@ filename="kn.pk"
 with open(path.join(filename),'rb') as f:
     lr=pickle.load(f)
     
-Have_At=st.number_input("Have_At")
-URL_Length=st.number_input("URL_Length")
-URL_Depth=st.number_input("URL_Depth")
+Have_At=st.number_input("Have At")
+URL_Length=st.number_input("URL Length")
+URL_Depth=st.number_input("URL Depth")
 TinyURL=st.number_input("TinyURL")
 Prefix_Suffix=st.number_input("Prefix/Suffix")
-DNS_Record=st.number_input("DNS_Record")
-Web_Traffic=st.number_input("Web_Traffic")
-Domain_Age=st.number_input("Domain_Age")
-Domain_End=st.number_input("Domain_End")
+DNS_Record=st.number_input("DNS Record")
+Web_Traffic=st.number_input("Web Traffic")
+Domain_Age=st.number_input("Domain Age")
+Domain_End=st.number_input("Domain End")
 iFrame=st.number_input("iFrame")
-Web_Forwards=st.number_input("Web_Forwards")
+Web_Forwards=st.number_input("Web Forwards")
 if st.button("predict") :
     pred=lr.predict(np.array([[Have_At,URL_Length,URL_Depth,TinyURL,Prefix_Suffix,DNS_Record,Web_Traffic,Domain_Age,Domain_End,iFrame,Web_Forwards]]))
     if pred==1:
         st.write("the website is phishing")
     else :
-        st.write("the website is not phishing")
+        st.write("the website is legitimate")
